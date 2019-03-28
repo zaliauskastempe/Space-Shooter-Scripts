4f0b3a67afe5946cdc279ec3ea70c4c52c2efd7e
zaliauskastempe <46976027+zaliauskastempe@users.noreply.github.com>
2019-03-27 23:47:19 -0400

    Add files via upload

---
+++ DestroyByTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByTime : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public float lifetime;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ randomRotator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomRotator : MonoBehaviour
+{
+
+    private Rigidbody rb;
+
+    public float tumble;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.angularVelocity = Random.insideUnitSphere * tumble;
+    }
+}
